b025cd83f66bbe9e31681b51c9680a33c0854681
Boonpin97 <pohboonpin123@hotmail.com>
2026-03-13 01:06:56 +0800

    Lightened colour of red coin

--- lib/params.dart
+++ lib/params.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 double playerbase = 0.0;
-const List<Color> coinColor = [Colors.redAccent, Colors.green, Colors.blue, Colors.yellow, Colors.orange, Colors.grey];
+const List<Color> coinColor = [Color.fromARGB(255, 245, 113, 113), Colors.green, Colors.blue, Colors.yellow, Colors.orange, Colors.grey];
 List<String> denominationList = ['0.1', '0.2', '1', '2', '5', '10'];
 //List<String> denominationList = [];
 
